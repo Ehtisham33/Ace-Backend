b1e3263bc376ed02be11a2a9457efdcbe863af10
Ehtisham33 <educatedshami@gmail.com>
2025-06-26 14:56:55 +0500

    all models done

--- ace_community/ace_community/settings.py
+++ ace_community/ace_community/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'rest_framework',
     'rest_framework.authtoken',
+    'laravel_models',
+    'chat',
 ]
 
 REST_FRAMEWORK = {
@@ -85,9 +87,13 @@ WSGI_APPLICATION = "ace_community.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ace_db',
+        'USER': 'root',
+        'PASSWORD': '12345',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
