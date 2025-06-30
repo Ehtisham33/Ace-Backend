7d6cb527f4d742cb12ce93ee6da6a67fa18c23f7
Ehtisham33 <educatedshami@gmail.com>
2025-06-30 18:26:10 +0500

    link user and set migrations

--- ace_community/chat/models.py
+++ ace_community/chat/models.py
@@ -1,18 +1,24 @@
 from django.db import models
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
+from laravel_models.models import Users
 
 class Message(models.Model):
-    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
-    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
+    sender = models.ForeignKey(
+        Users, 
+        on_delete=models.CASCADE, 
+        related_name='sent_messages',
+        db_column='sender_id'
+    )
+    receiver = models.ForeignKey(
+        Users, 
+        on_delete=models.CASCADE, 
+        related_name='received_messages',
+        db_column='receiver_id'
+    )
     content = models.TextField(blank=True)
     file = models.FileField(upload_to='chat_files/', null=True, blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
 
     class Meta:
+        db_table = 'chat_message'  # optional
         ordering = ['timestamp']
-
-    def __str__(self):
-        return f"{self.sender} ➝ {self.receiver}: {self.content[:30]}"
